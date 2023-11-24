21273c43b78f2e0b7111a59527e057099e8e6fb0
Paul Lampe <paul@mwlampe.de>
2023-11-24 15:41:46 +0100

    Add more tests and remove dict

--- test/test-cases/TestArrow.cpp
+++ test/test-cases/TestArrow.cpp
@@ -50,30 +50,8 @@ TEST(Arrow, SingleIntegerColumnONE_VALUE) {
 }
 
 
-TEST(Arrow, SingleIntegerColumnDICT) {
-  BtrBlocksConfig::get().integers.schemes = {IntegerSchemeType::UNCOMPRESSED, IntegerSchemeType::ONE_VALUE, IntegerSchemeType::DICT};
-  SchemePool::refresh();
-  int64_t numRows = 1000000;
-  arrow::Int32Builder builder;
-  for (int i = 0; i < numRows; ++i) {
-    arrow::Status s = builder.Append(i % 1000);
-  }
 
-  arrow::Status s = builder.AppendEmptyValue();
-
-  std::shared_ptr<arrow::Array> array = builder.Finish().ValueOrDie();
-
-  std::shared_ptr<arrow::Schema> schema =
-      arrow::schema({arrow::field("int_col", arrow::int32())});
-
-  std::shared_ptr<arrow::RecordBatch> batch = arrow::RecordBatch::Make(schema,numRows, {array});
-
-  ArrowTableCompressor::compress(batch);
-}
-
-
-
-TEST(Arrow, SingleIntegerColumnDICT) {
+TEST(Arrow, SingleIntegerColumnBP) {
   BtrBlocksConfig::get().integers.schemes = {IntegerSchemeType::UNCOMPRESSED, IntegerSchemeType::ONE_VALUE, IntegerSchemeType::BP};
   SchemePool::refresh();
   int64_t numRows = 1000000;
@@ -94,13 +72,13 @@ TEST(Arrow, SingleIntegerColumnDICT) {
   ArrowTableCompressor::compress(batch);
 }
 
-TEST(Arrow, SingleIntegerColumnDICT) {
+TEST(Arrow, SingleIntegerColumnFOR) {
   BtrBlocksConfig::get().integers.schemes = {IntegerSchemeType::UNCOMPRESSED, IntegerSchemeType::ONE_VALUE, IntegerSchemeType::FOR};
   SchemePool::refresh();
   int64_t numRows = 1000000;
   arrow::Int32Builder builder;
   for (int i = 0; i < numRows; ++i) {
-    arrow::Status s = builder.Append();
+    arrow::Status s = builder.Append((i % 100) + 2e7);
   }
 
   arrow::Status s = builder.AppendEmptyValue();
