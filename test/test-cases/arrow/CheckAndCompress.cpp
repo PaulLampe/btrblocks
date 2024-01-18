8f1c9a138cee94348623093434c7c5d6ffbe16a7
Paul Lampe <paul@mwlampe.de>
2024-01-18 09:47:32 +0100

    Remove debugging function

--- test/test-cases/arrow/CheckAndCompress.cpp
+++ test/test-cases/arrow/CheckAndCompress.cpp
@@ -3,19 +3,6 @@
 #include "arrow/ArrowTableCompressor.hpp"
 #include "test-cases/TestHelper.hpp"
 
-void compareColumns(const std::shared_ptr<arrow::ChunkedArray>& originalColumn, const std::shared_ptr<arrow::ChunkedArray>& comparedColumn) {
-    for (int i = 1; i < originalColumn->length() - 1; ++i) {
-        auto oVal = originalColumn->GetScalar(i).ValueOrDie()->ToString();
-        auto cVal = comparedColumn->GetScalar(i).ValueOrDie()->ToString();
-
-        auto oValAfter = originalColumn->GetScalar(i).ValueOrDie()->ToString();
-        auto cValAfter = comparedColumn->GetScalar(i).ValueOrDie()->ToString();
-
-        auto oValBefore = originalColumn->GetScalar(i).ValueOrDie()->ToString();
-        auto cValBefore = comparedColumn->GetScalar(i).ValueOrDie()->ToString();
-    }
-}
-
 void printAverageCompressionRatio(
     std::shared_ptr<vector<tuple<OutputBlockStats, vector<u8>>>>& output) {
   double compressionRatioSum =
@@ -37,8 +24,6 @@ void checkCompressTable(std::shared_ptr<arrow::Table>& table) {
   for (int i = 0; i < table->schema()->num_fields(); ++i) {
     auto originalColumn = table->column(i);
     auto decompressedColumn = decompressedOutput->column(i);
-
-    // compareColumns(originalColumn, decompressedColumn); // Just for debugging purposes
 
     assert(originalColumn->Equals(decompressedColumn));
   }
